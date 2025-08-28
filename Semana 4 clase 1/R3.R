9b2bb427efad440daea7ebd255a25b1847506a0a
Oliver <oliverberlanga01@gmail.com>
2025-08-28 09:29:54 -0600

    update

--- Semana 4 clase 1/R3.R
+++ Semana 4 clase 1/R3.R
@@ -33,3 +33,82 @@ library("ggplot2")
 ggplot(Calidad.de.planta, aes(x=IE,color=Tratamiento))+
   geom_density()
 
+sd(IE,"na.rm"=FALSE)
+
+
+#Vamos a separar los datos por tratamiento
+
+df_ctlr <- subset(Calidad.de.planta, Tratamiento == "Ctrl")
+df_fert <- subset(Calidad.de.planta, Tratamiento != "Ctrl")
+
+
+#Vamos a revisar la normalidad de los datos
+
+par(mfrow=c(1,2)) #Este es para poder ver al mismo tiempo los dos graficos de abajo, le estoy diciendo que quiero una fila y dos columnas
+qqnorm(df_ctlr$IE);qqline(df_ctlr$IE) 
+qqnorm(df_fert$IE);qqline(df_fert$IE)
+
+par(mfrow=c(1,1)) #Este es para volver a la normalidad, le estoy diciendo que quiero una fila y una columna
+
+
+shapiro.test(df_ctlr$IE)
+shapiro.test(df_fert$IE)
+
+#vamos a revisar las varianzas         ##    
+#################################         ##     
+var.test(IE ~ Tratamiento)      #   #####   ## 
+var.test(df_ctlr$IE,df_fert$IE) #   #####   ##   Se puede de cualquiera de las dos formas
+#################################         ##
+                                       ##    
+
+#Aplicar la prueba de t, varianzas iguales
+#dos colas=two.sided
+
+t.test(IE~Tratamiento,var.equal=TRUE,
+       alternative="two.sided")
+
+cohens_efecto <- function(x,y) {
+  n1 <- length(x); n2 <- length(y) 
+  s1 <- sd(x);   s2 <- sd(y)
+  sp <- sqrt(((n1-1) * s1^2 + (n2-1) * s2^2) / (n1+n2-2))
+  (mean(x)-mean(y))/sp } 
+
+d_cal <- cohens_efecto(df_ctlr$IE, df_fert$IE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
